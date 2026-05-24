9fde88400cc48934485fa9aa682d97556f820460
Nithi <nithitha.k2022@vitstudent.ac.in>
2026-05-24 18:28:57 +0530

    Added homepage UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,67 +2,35 @@ export default function Home() {
   return (
     <main
       style={{
-        fontFamily: "Arial, sans-serif",
         padding: "40px",
-        maxWidth: "900px",
-        margin: "0 auto",
+        fontFamily: "Arial",
+        backgroundColor: "#f4f4f4",
+        minHeight: "100vh",
       }}
     >
-      <h1 style={{ fontSize: "40px", marginBottom: "10px" }}>
+      <h1 style={{ color: "#111", fontSize: "40px" }}>
         Inventory Reservation System
       </h1>
 
-      <p style={{ fontSize: "18px", marginBottom: "30px" }}>
-        Backend API assignment built using Next.js, Prisma, PostgreSQL, and
-        Vercel.
+      <p style={{ marginTop: "20px", fontSize: "18px" }}>
+        Backend APIs are successfully deployed on Vercel.
       </p>
 
       <div
         style={{
-          border: "1px solid #ccc",
+          marginTop: "30px",
           padding: "20px",
-          borderRadius: "10px",
-          marginBottom: "20px",
-        }}
-      >
-        <h2>Create Reservation API</h2>
-
-        <p>
-          <strong>POST</strong> /api/reservations
-        </p>
-
-        <pre
-          style={{
-            background: "#f4f4f4",
-            padding: "10px",
-            borderRadius: "8px",
-          }}
-        >
-{`{
-  "productId": "cmpjk3ulg00006vvxmtlxfbhg",
-  "warehouseId": "cmpjk3uut00026vvx0juq0lvg",
-  "quantity": 2
-}`}
-        </pre>
-      </div>
-
-      <div
-        style={{
-          border: "1px solid #ccc",
-          padding: "20px",
+          background: "white",
           borderRadius: "10px",
         }}
       >
-        <h2>Confirm Reservation API</h2>
+        <h2>Available APIs</h2>
 
-        <p>
-          <strong>POST</strong> /api/reservations/:id/confirm
-        </p>
+        <ul>
+          <li>POST /api/reservations</li>
+          <li>POST /api/reservations/:id/confirm</li>
+        </ul>
       </div>
-
-      <p style={{ marginTop: "40px", color: "gray" }}>
-        Deployment successful on Vercel.
-      </p>
     </main>
   );
 }
